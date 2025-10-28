ea93fc3cac26e6df2f524468f83f07e00a684237
ben <benvansleen@gmail.com>
2025-10-28 18:23:28 -0400

    migrate `opts_and_keys` to fnl

--- fnl/config.fnl
+++ fnl/config.fnl
@@ -6,39 +6,79 @@
   (lze.register_handlers lzUtils.for_cat)
   (lze.register_handlers lzextras.lsp))
 
-(let [numbertoggle (vim.api.nvim_create_augroup :numbertoggle {})
+
+(let [numbertoggle-g (vim.api.nvim_create_augroup :numbertoggle {})
+      highlight-g (vim.api.nvim_create_augroup :highlight {})
       screen-width (vim.api.nvim_win_get_width 0)
       statuscolumn "  %l%s%C"
       statuscolumn-wide (.. (string.rep " " (/ (- screen-width 100) 3))
                             statuscolumn)]
-  (config {requires [:myLuaConf.opts_and_keys
-                     :myLuaConf.plugins
+  (vim.diagnostic.config {:virtual_lines true})
+  (config {g {mapleader " "
+              maplocalleader " "
+              my_center_buffer false
+              netrw_liststyle 0
+              netrw_banner 0}
+
+           requires [:myLuaConf.plugins
                      :myLuaConf.LSPs
                      :plugins]
-           opt {fillchars {:eob " "}
-                laststatus 0
-                number false
-                relativenumber false
-                ruler false
-                showmode false
-                showcmd false
-                statuscolumn statuscolumn
-                statusline "%{repeat('─',winwidth('.'))}"}
-           g {my_center_buffer false}
-           nmap {:<leader>tn (fn []
+            opt {breakindent true
+                 clipboard "unnamedplus"
+                 fillchars {:eob " "}
+                 hlsearch true
+                 inccommand "split"
+                 laststatus 0
+                 list true
+                 listchars {:tab "» "
+                            :trail "·"
+                            :nbsp "␣"}
+                 mouse "a"
+                 number false
+                 relativenumber false
+                 ruler false
+                 scrolloff 10
+                 showcmd false
+                 showmode false
+                 signcolumn :yes
+                 smartcase true
+                 statuscolumn statuscolumn
+                 statusline "%{repeat('─',winwidth('.'))}"
+                 termguicolors true
+                 timeoutlen 300
+                 updatetime 250
+                 undofile true}
+
+           nmap {:<Esc> "<cmd>nohlsearch<CR>"
+                 :<C-j> "<C-d>zz"
+                 :<C-k> "<C-u>zz"
+                 :<leader>tn (fn []
                                (let [cur vim.wo.nu]
                                  (set vim.wo.number (not cur))
                                  (set vim.wo.relativenumber (not cur))))
                  :<leader>tc (fn []
                                (set vim.g.my_center_buffer
                                     (not vim.g.my_center_buffer)))
+                 :<leader>te (fn []
+                              (let [vt (. (vim.diagnostic.config) "virtual_lines")]
+                               (vim.diagnostic.config {:virtual_lines (not vt)})))
                  :<leader>wtf (fn [] (print (vim.api.nvim_buf_get_name 0)))
                  :<leader>q (fn [] (vim.api.nvim_buf_delete 0 {}))}
+
            imap {:jj :<ESC>}
+
+           vmap {:J ":m '>+1<CR>gv=gv"
+                 :K ":m '>-2<CR>gv=gv"}
+
            autocmd {[:BufWinEnter]
                     {:desc "return cursor to where it was last time file was closed"
                      :pattern "*"
                      :command "silent! normal! g`\"zv"}
+
+                    [:TextYankPost]
+                    {:group highlight-g
+                     :pattern "*"
+                     :callback (fn [] (vim.highlight.on_yank))}
 
                     [:BufEnter :BufWinEnter :BufWinLeave :WinEnter :WinLeave :WinResized :VimResized]
                     {:callback (fn []
@@ -52,7 +92,7 @@
 
                     [:BufEnter :FocusGained :InsertLeave :CmdlineLeave :WinEnter]
                     {:pattern "*"
-                     :group numbertoggle
+                     :group numbertoggle-g
                      :once false
                      :nested false
                      :callback (fn []
@@ -62,7 +102,7 @@
 
                     [:BufLeave :FocusLost :InsertEnter :CmdlineEnter :WinLeave]
                     {:pattern "*"
-                     :group numbertoggle
+                     :group numbertoggle-g
                      :once false
                      :nested false
                      :callback (fn [] (when vim.wo.nu (set vim.wo.relativenumber false)))}}}))
--- lua/config.lua
+++ lua/config.lua
@@ -7,33 +7,72 @@ do
 	lze.register_handlers(lzextras.lsp)
 end
 do
-	local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
+	local numbertoggle_g = vim.api.nvim_create_augroup("numbertoggle", {})
+	local highlight_g = vim.api.nvim_create_augroup("highlight", {})
 	local screen_width = vim.api.nvim_win_get_width(0)
 	local statuscolumn = "  %l%s%C"
 	local statuscolumn_wide = (string.rep(" ", ((screen_width - 100) / 3)) .. statuscolumn)
+	vim.diagnostic.config({ virtual_lines = true })
+	vim.g["mapleader"] = " "
+	vim.g["maplocalleader"] = " "
+	vim.g["netrw_liststyle"] = 0
+	vim.g["netrw_banner"] = 0
+	vim.g["my_center_buffer"] = false
 	do
-		local _ = {
-			require("myLuaConf.opts_and_keys"),
-			require("myLuaConf.plugins"),
-			require("myLuaConf.LSPs"),
-			require("plugins"),
-		}
+		local _ = { nil, nil, nil, nil, nil }
 	end
+	do
+		local _ = { require("myLuaConf.plugins"), require("myLuaConf.LSPs"), require("plugins") }
+	end
+	vim.opt["breakindent"] = true
+	vim.opt["clipboard"] = "unnamedplus"
 	vim.opt["fillchars"] = { eob = " " }
+	vim.opt["hlsearch"] = true
+	vim.opt["inccommand"] = "split"
 	vim.opt["laststatus"] = 0
+	vim.opt["list"] = true
+	vim.opt["listchars"] = { tab = "\194\187 ", trail = "\194\183", nbsp = "\226\144\163" }
+	vim.opt["mouse"] = "a"
+	vim.opt["scrolloff"] = 10
+	vim.opt["signcolumn"] = "yes"
+	vim.opt["smartcase"] = true
 	vim.opt["statuscolumn"] = statuscolumn
 	vim.opt["statusline"] = "%{repeat('\226\148\128',winwidth('.'))}"
+	vim.opt["termguicolors"] = true
+	vim.opt["timeoutlen"] = 300
+	vim.opt["updatetime"] = 250
+	vim.opt["undofile"] = true
 	vim.opt["number"] = false
 	vim.opt["relativenumber"] = false
 	vim.opt["ruler"] = false
 	vim.opt["showcmd"] = false
 	vim.opt["showmode"] = false
 	do
-		local _ = { nil, nil, nil, nil, nil, nil, nil, nil, nil }
-	end
-	vim.g["my_center_buffer"] = false
-	do
-		local _ = { nil }
+		local _ = {
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+			nil,
+		}
 	end
 	local function _1_()
 		local cur = vim.wo.nu
@@ -46,23 +85,37 @@ do
 		return nil
 	end
 	local function _3_()
-		return print(vim.api.nvim_buf_get_name(0))
+		local vt = vim.diagnostic.config().virtual_lines
+		return vim.diagnostic.config({ virtual_lines = not vt })
 	end
 	local function _4_()
+		return print(vim.api.nvim_buf_get_name(0))
+	end
+	local function _5_()
 		return vim.api.nvim_buf_delete(0, {})
 	end
 	do
 		local _ = {
+			vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>"),
+			vim.keymap.set("n", "<C-j>", "<C-d>zz"),
+			vim.keymap.set("n", "<C-k>", "<C-u>zz"),
 			vim.keymap.set("n", "<leader>tn", _1_),
 			vim.keymap.set("n", "<leader>tc", _2_),
-			vim.keymap.set("n", "<leader>wtf", _3_),
-			vim.keymap.set("n", "<leader>q", _4_),
+			vim.keymap.set("n", "<leader>te", _3_),
+			vim.keymap.set("n", "<leader>wtf", _4_),
+			vim.keymap.set("n", "<leader>q", _5_),
 		}
 	end
 	do
 		local _ = { vim.keymap.set("i", "jj", "<ESC>") }
 	end
-	local function _5_()
+	do
+		local _ = { vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv"), vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") }
+	end
+	local function _6_()
+		return vim.highlight.on_yank()
+	end
+	local function _7_()
 		local winwidth = vim.api.nvim_win_get_width(0)
 		if vim.g.my_center_buffer and (winwidth > (screen_width / 3)) then
 			vim.wo.statuscolumn = statuscolumn_wide
@@ -72,7 +125,7 @@ do
 			return nil
 		end
 	end
-	local function _7_()
+	local function _9_()
 		if vim.wo.nu and ("i" ~= vim.api.nvim_get_mode().mode) then
 			vim.wo.relativenumber = true
 			return nil
@@ -80,7 +133,7 @@ do
 			return nil
 		end
 	end
-	local function _9_()
+	local function _11_()
 		if vim.wo.nu then
 			vim.wo.relativenumber = false
 			return nil
@@ -95,17 +148,18 @@ do
 				pattern = "*",
 				command = 'silent! normal! g`"zv',
 			}),
+			vim.api.nvim_create_autocmd({ "TextYankPost" }, { group = highlight_g, pattern = "*", callback = _6_ }),
 			vim.api.nvim_create_autocmd(
 				{ "BufEnter", "BufWinEnter", "BufWinLeave", "WinEnter", "WinLeave", "WinResized", "VimResized" },
-				{ callback = _5_ }
+				{ callback = _7_ }
 			),
 			vim.api.nvim_create_autocmd(
 				{ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" },
-				{ pattern = "*", group = numbertoggle, callback = _7_, nested = false, once = false }
+				{ pattern = "*", group = numbertoggle_g, callback = _9_, nested = false, once = false }
 			),
 			vim.api.nvim_create_autocmd(
 				{ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" },
-				{ pattern = "*", group = numbertoggle, callback = _9_, nested = false, once = false }
+				{ pattern = "*", group = numbertoggle_g, callback = _11_, nested = false, once = false }
 			),
 		}
 	end
